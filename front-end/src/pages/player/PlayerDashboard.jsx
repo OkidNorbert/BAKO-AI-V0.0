4117df773c85b6ab85d72915e82c2c63670e5ca6
a-kum2580 <132387920+a-kum2580@users.noreply.github.com>
2026-02-01 15:35:30 +0300

    ....

--- front-end/src/pages/player/PlayerDashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CoachDashboard from '../../components/player/coach-dashboard';
-
-const CoachDashboardPage = () => {
-  return <CoachDashboard />;
-};
-
-export default CoachDashboardPage; 
--- front-end/src/pages/team/Management/TeamDashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TeamDashboard from '../../components/team/team-dashboard';
-
-const TeamDashboardPage = () => {
-  return <TeamDashboard />;
-};
-
-export default TeamDashboardPage; 
